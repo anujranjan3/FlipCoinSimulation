8058475ea940fbbb8f0f9ccb78ca8a04ca9c815b
unknown <ararky7@gmail.com>
2020-10-06 22:25:41 +0530

    win or tie program

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,9 +1,8 @@
 #!/bin/bash -x
 
-read -p "Enter the number of times you want to flip coin : " counter;
 tails=0;
 heads=0;
-for (( i=0; i<$counter; i++ ))
+while [ $tails -lt 21 -a $heads -lt 21 ]
 do
 randomNum=$((RANDOM%2));
 if [ $randomNum -eq 0 ];
@@ -15,4 +14,16 @@ else
 	echo Heads;
 fi
 done
+if [ $heads -gt $tails ]
+then
+	win=$(($heads-$tails))
+	echo "Heads won by : "$win;
+elif [ $heads -eq $tails ]
+then
+	echo "It's a tie";
+else
+	win=$(($tails-$heads));
+	echo "Tails won by : "$win;
+fi
+
 echo "Heads Count :"$heads "and Tails count : " $tails;
